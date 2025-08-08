3e9f061227d551f3c39ee6eed17b9c5bea5401db
Hasib Azami <alexazami1993@gmail.com>
2025-08-08 10:28:31 -0700

    Add files via upload

---
+++ netflix_schema.sql
@@ -0,0 +1,19 @@
+-- Netflix Project
+
+CREATE TABLE netflix
+(
+show_id VARCHAR(5),
+type VARCHAR(10),
+title VARCHAR(150),
+director VARCHAR(208),
+casts VARCHAR(1000),
+country VARCHAR(150),
+date_added VARCHAR(50),
+release_year VARCHAR(10),
+rating VARCHAR(10),
+duration VARCHAR(15),
+listed_in VARCHAR(10),
+description VARCHAR(250)
+
+);
+
